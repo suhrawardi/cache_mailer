5811880ba5711dde70d164343392c34309cb5072
jarra <suhrawardi@gmail.com>
2010-10-10 01:32:57 +0200

    Fixing dependency.

--- lib/cache_mailer.rb
+++ lib/cache_mailer.rb
@@ -1,6 +1,5 @@
 require 'action_mailer'
 require 'mail/cache_mailer'
-require 'cache_mailer/cucumber'
 
 Mail::CacheMailer.clear_cache!
 ActionMailer::Base.add_delivery_method :cache, Mail::CacheMailer
